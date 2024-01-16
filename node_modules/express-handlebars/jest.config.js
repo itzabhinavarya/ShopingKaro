96c37eb8fadd7e5cfbc9328644b1d17c8661b6d4
itzabhinavarya <itzabhinavarya@gmail.com>
2024-01-16 20:53:01 +0530

    adding gitignore

---
+++ node_modules/express-handlebars/jest.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+	preset: "ts-jest",
+	testEnvironment: "node",
+	restoreMocks: true,
+	clearMocks: true,
+	collectCoverageFrom: [
+		"lib/**/*.ts",
+	],
+	coverageDirectory: "coverage",
+	coverageThreshold: {
+		global: {
+			branches: 100,
+			functions: 100,
+			lines: 100,
+			statements: 100,
+		},
+	},
+	testRegex: /\.test\.tsx?/.source,
+	transform: {
+		[/\.test\.tsx?/.source]: [
+			"ts-jest", {
+				diagnostics: false,
+			},
+		],
+	},
+	moduleFileExtensions: ["js", "json", "jsx", "d.ts", "ts", "tsx", "node"],
+};
